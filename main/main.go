809772ae7154306d1c313a1c7308e4bb8085e4d9
SoliDeoHoneretGloria <iphone_1st@naver.com>
2023-05-01 20:18:23 +0200

    response body was implemented as go channel

--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -22,7 +22,7 @@ var calling2 Calling
 var ad int64
 var xid string
 
-const numRequests = 100
+const numRequests = 1000
 
 type Calling struct {
 	// defining struct variables
@@ -37,6 +37,7 @@ func main() {
 	c1 := make(chan string)
 	timeActs := make(chan int64)
 	signal := make(chan interface{})
+	resCh := make(chan *http.Response)
 
 	adjust()
 	time.Sleep(1000)
@@ -46,9 +47,12 @@ func main() {
 
 	for i := 0; i < numRequests; i++ {
 		wait.Add(1)
-		go put(timeActs, signal, wait)
+		go put(resCh, signal)
 	}
+
+	go parshing(resCh, timeActs, wait)
 	go sleeping(timeActs, signal)
+
 	wait.Wait()
 }
 
@@ -57,7 +61,7 @@ func adjust() {
 	var sum int64 = 0
 	for i := 0; i <= 1; i++ {
 		resp, _ := http.PostForm(urlCommon, url.Values{"nickname": {"dummy"}})
-		respBody, _ := ioutil.ReadAll(resp.Body)
+		respBody, _ := io.ReadAll(resp.Body)
 		err1 := json.Unmarshal(respBody, &calling2)
 		if err1 != nil {
 			fmt.Fprintln(w, err1)
@@ -71,13 +75,11 @@ func adjust() {
 	ad = sum
 }
 
-//ポストで挑戦、スタート
+// ポストで挑戦、スタート
 func post(timeAct chan int64, c1 chan string) {
 	defer w.Flush()
-
 	resp, err := http.PostForm(urlCommon, url.Values{"nickname": {"testuser"}})
-
-	respBody, err := ioutil.ReadAll(resp.Body)
+	respBody, err := io.ReadAll(resp.Body)
 	if err == nil {
 		str := string(respBody)
 		fmt.Fprintln(w, str)
@@ -91,46 +93,51 @@ func post(timeAct chan int64, c1 chan string) {
 	timeAct <- calling1.Actives_at
 }
 
-//反復フット進行
-func put(timeActs chan int64, signal chan interface{}, wait *sync.WaitGroup) {
-	defer wait.Done()
-	defer w.Flush()
-
-	req, err := http.NewRequest(http.MethodPut, urlCommon, nil)
+// 反復フット進行
+func put(resCh chan *http.Response, signal chan interface{}) {
+	req, _ := http.NewRequest(http.MethodPut, urlCommon, nil)
 	req.Header.Set("X-Challenge-Id", xid)
 	client := &http.Client{}
 	//シグナルによる待ち時間制御
 	<-signal
 	resp, _ := client.Do(req)
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err == nil {
-		str := string(respBody)
-		fmt.Fprintln(w, str)
-		if strings.Contains(str, "result") {
-			fmt.Println(str)
-			os.Exit(1)
-		}
-	}
-	err1 := json.Unmarshal(respBody, &calling1)
-	if err1 != nil {
-		fmt.Fprintln(w, err1)
-	}
-	defer resp.Body.Close()
-	called = calling1.Called_at
-	timeActs <- calling1.Actives_at
-
+	resCh <- resp
 }
 
-//補正時間を加えたスリーピング時間
+// 補正時間を加えたスリーピング時間
 func sleeping(timeActs <-chan int64, signal chan interface{}) {
 	for timeAct := range timeActs {
-		sleepTime := timeAct - called + ad
+		sleepTime := timeAct - called + ad - 10
 		if sleepTime <= 0 {
 			signal <- struct{}{}
 			continue
 		}
 		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
 		signal <- struct{}{}
+	}
+}
+
+func parshing(resCh chan *http.Response, timeActs chan int64, wait *sync.WaitGroup) {
+	for res := range resCh {
+		resp := res
+		respBody, err := io.ReadAll(resp.Body)
+		if err == nil {
+			str := string(respBody)
+			fmt.Fprintln(w, str)
+			if strings.Contains(str, "result") {
+				fmt.Println(str)
+				os.Exit(1)
+			}
+		}
+		err1 := json.Unmarshal(respBody, &calling1)
+		if err1 != nil {
+			fmt.Fprintln(w, err1)
+		}
+		called = calling1.Called_at
+		timeActs <- calling1.Actives_at
+
+		w.Flush()
+		wait.Done()
+		resp.Body.Close()
 	}
 }
